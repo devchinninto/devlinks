81704d28943e17e78d3254068e50b4f7dfcd9046
Marcelle Alves <marcellealves018@gmail.com>
2025-08-24 18:06:44 -0300

    added meta tag with theme color

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
+  
+  const themeColorMeta = document.querySelector('meta[name="theme-color"]')
 
-
-  // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  // substituir a img
   if (html.classList.contains('light')) {
-    //se tiver light mode, adc a img light
     img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute(
+      "alt",
+      "Foto de Marcelle Alves sorrindo, usando óculos escuro preto e camisa preta, e o fundo da imagem é um cinza escuro."
+    )
+    themeColorMeta.setAttribute('content', '#ffffff80')
   } else {
-    // se tiver sem light mode, manter a img normal
     img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute(
+      "alt",
+      "Desenho de Marcelle Alves sorrindo, usando óculos com armação transparente e camisa preta, e o fundo da imagem amarelo."
+    )
+    themeColorMeta.setAttribute('content', '#ffffff00')
   }
-
-  if (html.classList.contains('light')) {
-    img.setAttribute('alt', 'Foto de Marcelle Alves sorrindo, usando óculos escuro preto e camisa preta, e o fundo da imagem é um cinza escuro.')
-  } else { 
-    img.setAttribute('alt', 'Desenho de Marcelle Alves sorrindo, usando óculos com armação transparente e camisa preta, e o fundo da imagem amarelo.')
-  }
-
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const openPopupBtn = document.getElementById("open-popup-btn")
   const closePopupBtn = document.querySelector(".close-btn")
   const popupContainer = document.getElementById("download-popup")
+  const themeColorMeta = document.getElementById("theme-color-meta")
 
   if (openPopupBtn && closePopupBtn && popupContainer) {
-    // Abre o pop-up
     openPopupBtn.addEventListener("click", () => {
       popupContainer.classList.add("show")
     })
 
-    // Fecha o pop-up quando o 'x' é clicado
     closePopupBtn.addEventListener("click", () => {
       popupContainer.classList.remove("show")
     })
 
-    // Fecha o pop-up se o usuário clicar fora dele
     window.addEventListener("click", (event) => {
       if (event.target === popupContainer) {
         popupContainer.classList.remove("show")
       }
     })
 
-    // Fecha o pop-up ao apertar a tecla 'Esc'
     document.addEventListener("keydown", (event) => {
       if (event.key === "Escape") {
         popupContainer.classList.remove("show")
