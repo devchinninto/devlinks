4604ba234b8aa408397e539c16c92c884c1c57d8
Marcelle Alves <marcellealves018@gmail.com>
2025-11-02 19:26:54 -0300

    upd: profile pic updated

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/pfp-outdoor.png")
+    img.setAttribute("src", "./assets/pfp.png")
   } else {
-    img.setAttribute("src", "./assets/pfp-outdoor.png")
+    img.setAttribute("src", "./assets/pfp.png")
   }
 
   if (html.classList.contains("light")) {
