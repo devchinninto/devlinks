4a546200ef4e5e14a0724833cc01933b52a32f94
Marcelle Alves <marcellealves018@gmail.com>
2025-11-02 19:17:59 -0300

    feat(update) atualização de links

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/pfp-outdoor.png")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/pfp-outdoor.png")
   }
 
   if (html.classList.contains("light")) {
